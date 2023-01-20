bb1d35a2d4be86a153cfa9e73e95e7c0c2a324a2
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-20 16:30:32 +0100

    :green_heart: Simple setup

--- src/Experience.js
+++ src/Experience.js
@@ -1,7 +1,19 @@
+import { Center, OrbitControls } from '@react-three/drei';
 import React from 'react';
 
 const Experience = () => {
-    return <div>Experience</div>;
+    return (
+        <>
+            <OrbitControls />
+
+            <Center>
+                <mesh>
+                    <boxGeometry />
+                    <meshBasicMaterial color="mediumpurple" />
+                </mesh>
+            </Center>
+        </>
+    );
 };
 
 export default Experience;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Experience from './Experience';
 import './App.css';
+import { Canvas } from '@react-three/fiber';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Experience />
+        <Canvas
+            flat
+            camera={{
+                fov: 45,
+                near: 0.1,
+                far: 200,
+                position: [1, 2, 6]
+            }}
+        >
+            <Experience />
+        </Canvas>
     </React.StrictMode>
 );
