ad746de211d21a89142d01fdc6ac71290b9dca4b
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-23 11:21:55 +0100

    :monocle_face: Light Helper

--- src/components/Lights.js
+++ src/components/Lights.js
@@ -1,17 +1,26 @@
+import { useRef } from 'react';
+import { DirectionalLightHelper } from 'three';
+import { useHelper } from '@react-three/drei';
 import { useControls } from 'leva';
 
 const Lights = () => {
+    const directionalLight = useRef();
+    useHelper(directionalLight, DirectionalLightHelper, 0.5);
+
     const { position } = useControls('Lights', {
         position: {
-            value: { x: 2, y: 1, z: 1 },
+            value: { x: 2, y: 0.5, z: -2 },
             step: 0.01,
             joystick: 'invertY'
         }
     });
     return (
         <>
-            <ambientLight />
-            <directionalLight position={[position.x, position.y, position.z]} />
+            <directionalLight
+                intensity={0.5}
+                ref={directionalLight}
+                position={[position.x, position.y, position.z]}
+            />
         </>
     );
 };
