7336a2a6f2d1630bcba7719dd9bd3b3b2951d187
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-25 19:46:21 +0100

    :sparkles: Limit for camera

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -4,7 +4,18 @@ import { useThree } from '@react-three/fiber';
 const Camera = () => {
     const { camera, gl } = useThree();
 
-    return <OrbitControls camera={camera} gl={gl} />;
+    return (
+        <OrbitControls
+            camera={camera}
+            gl={gl}
+            rotateSpeed={0.25}
+            zoomSpeed={0.25}
+            minPolarAngle={0}
+            maxPolarAngle={Math.PI / 2}
+            minAzimuthAngle={Math.PI / 2}
+            maxAzimuthAngle={Math.PI}
+        />
+    );
 };
 
 export default Camera;
