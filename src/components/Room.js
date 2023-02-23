72e3d3ee21e465a97e5b50a96cafec7eaeadb4ec
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-02-23 15:13:26 +0100

    :lipstick: Baking update

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,9 +1,11 @@
 import StaticRoom from './elements/StaticRoom';
+import Plant from './elements/animated/Plant';
 
 const Room = () => {
     return (
         <group>
             <StaticRoom />
+            <Plant />
         </group>
     );
 };
