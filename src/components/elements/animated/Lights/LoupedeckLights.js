164d97a45d7bba8a0af10cb9a622ed877dc21d68
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-02-27 15:38:36 +0100

    :dizzy: Animated loupedeck lights

--- src/components/elements/animated/Lights/LoupedeckLights.js
+++ src/components/elements/animated/Lights/LoupedeckLights.js
@@ -1,18 +1,50 @@
 import { useRef } from 'react';
-import { useFrame } from '@react-three/fiber';
+import { useFrame, useThree } from '@react-three/fiber';
 import { useGLTF } from '@react-three/drei';
+import { useState, useEffect } from 'react';
 
 /**
  * Loupedeck
  */
 const LoupedeckLights = () => {
-    const light1 = useRef();
+    // model
     const { nodes } = useGLTF('./assets/lights/loupedeckLights.glb');
-    console.log('🚀 ~ LoupedeckLights ~ nodes:', nodes);
 
-    // Lights Animations
-    useFrame((_, delta) => {
-        light1.current.emissiveIntensity = Math.sin(1);
+    const light1 = useRef();
+    const light2 = useRef();
+    const light3 = useRef();
+    const light4 = useRef();
+    const light5 = useRef();
+    const light6 = useRef();
+    const light7 = useRef();
+    const light8 = useRef();
+    const light9 = useRef();
+    const light10 = useRef();
+    const light11 = useRef();
+    const light12 = useRef();
+
+    const lights = [
+        light1,
+        light2,
+        light3,
+        light4,
+        light5,
+        light6,
+        light7,
+        light8,
+        light9,
+        light10,
+        light11,
+        light12
+    ];
+
+    /**
+     * Light Animation
+     */
+    useFrame(({ clock }) => {
+        lights.forEach((light) => {
+            light.current.material.emissiveIntensity = Math.sin(clock.getElapsedTime()) + 1;
+        });
     });
 
     return (
@@ -24,19 +56,19 @@ const LoupedeckLights = () => {
             >
                 <meshStandardMaterial
                     color="grey"
-                    emissive="darkorange"
-                    emissiveIntensity={1.25}
+                    emissive="white"
+                    emissiveIntensity={1}
                     toneMapped={false}
                 />
             </mesh>
 
             {/* 1 */}
             <mesh
+                ref={light1}
                 geometry={nodes.LoupedeckLights001.geometry}
                 position={nodes.LoupedeckLights001.position}
             >
                 <meshStandardMaterial
-                    ref={light1}
                     color="grey"
                     emissive="orange"
                     emissiveIntensity={0}
@@ -44,34 +76,37 @@ const LoupedeckLights = () => {
                 />
             </mesh>
             <mesh
+                ref={light2}
                 geometry={nodes.LoupedeckLights002.geometry}
                 position={nodes.LoupedeckLights002.position}
             >
                 <meshStandardMaterial
                     color="grey"
-                    emissive="orange"
+                    emissive="white"
                     emissiveIntensity={0}
                     toneMapped={false}
                 />
             </mesh>
             <mesh
+                ref={light3}
                 geometry={nodes.LoupedeckLights003.geometry}
                 position={nodes.LoupedeckLights003.position}
             >
                 <meshStandardMaterial
                     color="grey"
-                    emissive="orange"
+                    emissive="khaki"
                     emissiveIntensity={0}
                     toneMapped={false}
                 />
             </mesh>
             <mesh
+                ref={light4}
                 geometry={nodes.LoupedeckLights004.geometry}
                 position={nodes.LoupedeckLights004.position}
             >
                 <meshStandardMaterial
                     color="grey"
-                    emissive="orange"
+                    emissive="violet"
                     emissiveIntensity={0}
                     toneMapped={false}
                 />
@@ -79,45 +114,49 @@ const LoupedeckLights = () => {
 
             {/* 2 */}
             <mesh
+                ref={light5}
                 geometry={nodes.LoupedeckLights005.geometry}
                 position={nodes.LoupedeckLights005.position}
             >
                 <meshStandardMaterial
                     color="grey"
-                    emissive="orange"
+                    emissive="pink"
                     emissiveIntensity={0}
                     toneMapped={false}
                 />
             </mesh>
             <mesh
+                ref={light6}
                 geometry={nodes.LoupedeckLights006.geometry}
                 position={nodes.LoupedeckLights006.position}
             >
                 <meshStandardMaterial
                     color="grey"
-                    emissive="orange"
+                    emissive="violet"
                     emissiveIntensity={0}
                     toneMapped={false}
                 />
             </mesh>
             <mesh
+                ref={light7}
                 geometry={nodes.LoupedeckLights007.geometry}
                 position={nodes.LoupedeckLights007.position}
             >
                 <meshStandardMaterial
                     color="grey"
-                    emissive="orange"
+                    emissive="cyan"
                     emissiveIntensity={0}
                     toneMapped={false}
                 />
             </mesh>
             <mesh
+                ref={light8}
                 geometry={nodes.LoupedeckLights008.geometry}
                 position={nodes.LoupedeckLights008.position}
             >
                 <meshStandardMaterial
                     color="grey"
-                    emissive="orange"
+                    emissive="lightgreen"
                     emissiveIntensity={0}
                     toneMapped={false}
                 />
@@ -125,28 +164,31 @@ const LoupedeckLights = () => {
 
             {/* 3 */}
             <mesh
+                ref={light9}
                 geometry={nodes.LoupedeckLights009.geometry}
                 position={nodes.LoupedeckLights009.position}
             >
                 <meshStandardMaterial
                     color="grey"
-                    emissive="orange"
+                    emissive="lightgreen"
                     emissiveIntensity={0}
                     toneMapped={false}
                 />
             </mesh>
             <mesh
+                ref={light10}
                 geometry={nodes.LoupedeckLights010.geometry}
                 position={nodes.LoupedeckLights010.position}
             >
                 <meshStandardMaterial
                     color="grey"
-                    emissive="orange"
+                    emissive="#5ebdd8"
                     emissiveIntensity={0}
                     toneMapped={false}
                 />
             </mesh>
             <mesh
+                ref={light11}
                 geometry={nodes.LoupedeckLights011.geometry}
                 position={nodes.LoupedeckLights011.position}
             >
@@ -158,12 +200,13 @@ const LoupedeckLights = () => {
                 />
             </mesh>
             <mesh
+                ref={light12}
                 geometry={nodes.LoupedeckLights012.geometry}
                 position={nodes.LoupedeckLights012.position}
             >
                 <meshStandardMaterial
                     color="grey"
-                    emissive="orange"
+                    emissive="yellow"
                     emissiveIntensity={0}
                     toneMapped={false}
                 />
