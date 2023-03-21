7a84d432fb94773686a0fa9269c0f4fbe54aafc4
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-03-21 17:39:02 +0100

    :dizzy: Open and close project folder

--- src/components/elements/sections/monitor/MonitorScreen.js
+++ src/components/elements/sections/monitor/MonitorScreen.js
@@ -1,42 +1,39 @@
 import React from 'react';
-import { useEffect } from 'react';
 import { useState } from 'react';
 import styled from 'styled-components';
 import useInteractions from '../../../../utils/stores/useInteractions';
-import { FcOpenedFolder } from 'react-icons/fc';
 import { FcFolder } from 'react-icons/fc';
 
 const MonitorScreen = () => {
     /**
      * Styled
      */
-    const ProjectApp = styled.div`
+    const Desktop = styled.div`
         width: 100%;
         height: 100%;
-        border-radius: 0.5rem;
+        border-radius: 2rem;
         background-color: #fefefe;
-        color: #333333;
-        transition: ease-in 0.1;
 
         .topbar {
-            height: 1rem;
+            height: 4.5rem;
             border-bottom: 1px solid #333333;
             display: flex;
             align-items: center;
             border-radius: 0.5rem 0.5rem 0 0;
             background-color: #333333;
             .circle {
-                width: 0.5rem;
-                height: 0.5rem;
-                margin: 0.25rem;
+                width: 2.5rem;
+                height: 2.5rem;
+                margin: 0 0.75rem;
                 border-radius: 99%;
 
                 &:first-of-type {
-                    margin-left: 0.5rem;
+                    margin-left: 1rem;
                 }
             }
 
             .red {
+                cursor: pointer;
                 background-color: red;
             }
             .yellow {
@@ -45,89 +42,20 @@ const MonitorScreen = () => {
             .green {
                 background-color: green;
             }
-        }
-        h1 {
-            text-align: center;
-            margin: 0.2rem 0 0 0;
-            font-size: 1rem;
-        }
-    `;
-
-    const Projects = styled.section`
-        width: 100%;
-        height: 80%;
-        padding: 0.1rem;
-        border-radius: 0.5rem;
-        display: flex;
-        justify-content: space-between;
-        flex-wrap: wrap;
-        overflow-y: auto;
-
-        &::-webkit-scrollbar {
-            width: 8px;
-            background-color: #fefefe;
-            border-radius: 0.5rem;
-        }
-
-        &::-webkit-scrollbar-thumb {
-            border-radius: 10px;
-            -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
-            background-color: #f44a02;
-        }
-    `;
-
-    const Project = styled.a`
-        width: 9.5rem;
-        height: 4.5rem;
-        background-color: #fefefe;
-        margin: 1rem 0.5rem;
-        border-radius: 0.5rem;
-        text-decoration: none;
-        display: flex;
-        flex-direction: column;
-
-        &:hover .title {
-            background-color: #ed8153;
-            transition-duration: 0.2s;
-        }
-
-        img {
-            width: 100%;
-            height: 100%;
-            border-radius: 0.5rem 0.5rem 0 0;
-        }
-
-        .title {
-            width: 100%;
-            padding: 0.15rem 0;
-            border-radius: 0 0 0.5rem 0.5rem;
-            background-color: #f44a02;
-            text-align: center;
-            font-size: 0.6rem;
-            color: #fefefe;
-            align-self: flex-end;
-        }
-    `;
-
-    const Desktop = styled.div`
-        width: 100%;
-        height: 100%;
-        border-radius: 2rem;
-        background-color: #fefefe;
-
-        .title {
-            text-align: center;
-            margin-top: 1rem;
-            margin-bottom: 1rem;
-            padding-bottom: 0.5rem;
-            border-bottom: 2px solid #333333;
-            font-size: 3rem;
-            font-weight: 1000;
+            .title {
+                margin-left: 40rem;
+                margin-top: 1rem;
+                margin-bottom: 1rem;
+                font-size: 3rem;
+                font-weight: 1000;
+                color: #fefefe;
+            }
         }
 
         .folders {
             width: 100%;
-            height: 90%;
+            height: calc(100% - 4.5rem);
+            padding-top: 1.5rem;
             display: flex;
 
             .list {
@@ -271,9 +199,14 @@ const MonitorScreen = () => {
         }
     `;
 
-    // In project phase show project app
-    const [selectedProject, setSelectedProject] = useState();
-    const _state = useInteractions((state) => state);
+    /**
+     * Is the project folder open?
+     */
+    const [isFolderOpen, setIsFolderOpen] = useState(true);
+
+    /**
+     * Array of projects
+     */
     const [projectsArray, setProjectsArray] = useState([
         {
             id: 1,
@@ -286,54 +219,81 @@ const MonitorScreen = () => {
         }
     ]);
 
+    /**
+     * Project is shown
+     */
+    const [selectedProject, setSelectedProject] = useState(projectsArray[0]);
+
+    /**
+     * On click change the selected project
+     */
     const handleClick = (prj) => {
         setSelectedProject(prj === selectedProject ? null : prj);
     };
 
     return (
         <div className="wrapper-monitor">
-            <Desktop className="montserrat">
-                <h1 className="title">Projects</h1>
-                <div className="folders">
-                    {/* List */}
-                    <div className="list">
-                        {projectsArray.map((prj) => {
-                            return (
-                                <div
-                                    key={prj.id}
-                                    className={`folder-wrapper ${
-                                        selectedProject === prj ? 'is-selected' : ''
-                                    }`}
-                                    onClick={() => handleClick(prj)}
-                                >
-                                    <FcFolder className="folder" />
-                                    <h2>{prj.name}</h2>
-                                </div>
-                            );
-                        })}
+            {isFolderOpen ? (
+                /**
+                 * Folder opened
+                 */
+                <Desktop className="montserrat">
+                    <div className="topbar">
+                        <div className="circle red" onClick={() => setIsFolderOpen(false)}></div>
+                        <div className="circle yellow"></div>
+                        <div className="circle green"></div>
+                        <h1 className="title">Projects</h1>
                     </div>
 
-                    {/* Description */}
-                    {selectedProject !== undefined && (
-                        <div className="description">
-                            <h1>{selectedProject.name}</h1>
-                            <img src={selectedProject.img} alt={selectedProject.name} />
-                            <div className="tags">
-                                {selectedProject.tec.map((tag) => {
-                                    return <span className={`tag ${tag}`}>#{tag}</span>;
-                                })}
-                            </div>
-                            <p>{selectedProject.description}</p>
-
-                            <button>
-                                <a href="" target="_blank">
-                                    Visit Website
-                                </a>
-                            </button>
+                    <div className="folders">
+                        {/* List */}
+                        <div className="list">
+                            {projectsArray.map((prj) => {
+                                return (
+                                    <div
+                                        key={prj.id}
+                                        className={`folder-wrapper ${
+                                            selectedProject === prj ? 'is-selected' : ''
+                                        }`}
+                                        onClick={() => handleClick(prj)}
+                                    >
+                                        <FcFolder className="folder" />
+                                        <h2>{prj.name}</h2>
+                                    </div>
+                                );
+                            })}
                         </div>
-                    )}
+
+                        {/* Description */}
+                        {selectedProject !== undefined && (
+                            <div className="description">
+                                <h1>{selectedProject.name}</h1>
+                                <img src={selectedProject.img} alt={selectedProject.name} />
+                                <div className="tags">
+                                    {selectedProject.tec.map((tag) => {
+                                        return <span className={`tag ${tag}`}>#{tag}</span>;
+                                    })}
+                                </div>
+                                <p>{selectedProject.description}</p>
+
+                                <button>
+                                    <a href="" target="_blank">
+                                        Visit Website
+                                    </a>
+                                </button>
+                            </div>
+                        )}
+                    </div>
+                </Desktop>
+            ) : (
+                /**
+                 * Folder Closed
+                 */
+                <div className="folder-closed" onClick={() => setIsFolderOpen(true)}>
+                    <FcFolder className="folder-icon" />
+                    <h1 className="montserrat">Projects</h1>
                 </div>
-            </Desktop>
+            )}
         </div>
     );
 };
