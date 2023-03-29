e933934124ac536f3891309194da6ec9e6d479e8
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-03-29 16:00:05 +0200

    :sparkles: Contacts screen

--- src/components/elements/sections/phone/Phone.js
+++ src/components/elements/sections/phone/Phone.js
@@ -1,8 +1,61 @@
-import { useGLTF } from '@react-three/drei';
+import { Html, useGLTF } from '@react-three/drei';
+import PhoneScreen from './PhoneScreen';
+import useInteractions from '../../../../utils/stores/useInteractions';
 
 const Phone = () => {
-    const iphone = useGLTF('./assets/models/iphone.glb');
-    return <primitive object={iphone.scene} />;
+    const { nodes } = useGLTF('./assets/models/iphone.glb');
+
+    const phase = useInteractions((state) => state.phase);
+
+    return (
+        <group>
+            {/* iPhone */}
+            {nodes.iPhone.children.map((el) => {
+                return (
+                    <mesh
+                        key={el.uuid}
+                        geometry={el.geometry}
+                        position={[
+                            nodes.iPhone.position.x,
+                            nodes.iPhone.position.y - 0.00075,
+                            nodes.iPhone.position.z
+                        ]}
+                        scale={nodes.iPhone.scale}
+                        rotation={nodes.iPhone.rotation}
+                        material={el.material}
+                    />
+                );
+            })}
+
+            {/* iPhone Screen */}
+            <mesh
+                geometry={nodes.iPhoneScreen.geometry}
+                position={[
+                    nodes.iPhoneScreen.position.x,
+                    nodes.iPhoneScreen.position.y - 0.001,
+                    nodes.iPhoneScreen.position.z
+                ]}
+                scale={nodes.iPhoneScreen.scale}
+                rotation={nodes.iPhoneScreen.rotation}
+            >
+                <meshBasicMaterial color="#333333" />
+
+                {phase === 'contacts' && (
+                    <Html
+                        fixed
+                        position={[0, 0, 0]}
+                        rotation={[Math.PI / 2, Math.PI, -0.33]}
+                        transform
+                        occlude
+                        distanceFactor={0.05}
+                        zIndexRange={2}
+                    >
+                        <PhoneScreen />
+                    </Html>
+                )}
+            </mesh>
+        </group>
+    );
 };
 
 export default Phone;
---
+++ src/components/elements/sections/phone/PhoneScreen.js
@@ -0,0 +1,138 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FaLinkedin } from 'react-icons/fa';
+import { FiMail, FiPhoneCall, FiGithub, FiDownload } from 'react-icons/fi';
+
+const PhoneScreen = () => {
+    const Screen = styled.div`
+        width: 400px;
+        height: 750px;
+        background-color: #222222;
+        display: flex;
+        flex-direction: column;
+        justify-content: end;
+
+        .notify {
+            width: 100%;
+            height: 100px;
+            background-color: rgba(255, 255, 255, 0.35);
+            border-radius: 1rem;
+            margin-bottom: 1rem;
+            display: flex;
+            align-items: center;
+            color: #fefefe;
+
+            .icon {
+                width: 100px;
+                height: 100px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+
+                i {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    border-radius: 1rem;
+                    padding: 0.5rem;
+                    font-size: 3.5rem;
+                }
+            }
+            .info {
+                width: calc(100% - 100px);
+                padding-left: 1rem;
+
+                .title {
+                    font-size: larger;
+                    font-weight: 600;
+                    color: #ffffff;
+                    margin-bottom: 0.5rem;
+                }
+
+                .description {
+                    font-weight: 300;
+
+                    span {
+                        font-weight: 600;
+                    }
+                }
+            }
+        }
+    `;
+
+    return (
+        <Screen className="montserrat">
+            <div className="notify">
+                <div className="icon">
+                    <i style={{ backgroundColor: '#ffffff', color: '#333333' }}>
+                        <FiDownload />
+                    </i>
+                </div>
+                <div className="info">
+                    <div className="title">Curriculum</div>
+                    <div className="description">
+                        Download my <span>Curriculum</span>
+                    </div>
+                </div>
+            </div>
+
+            <div className="notify">
+                <div className="icon">
+                    <i style={{ backgroundColor: '#56b3ee' }}>
+                        <FiMail />
+                    </i>
+                </div>
+                <div className="info">
+                    <div className="title">Mail</div>
+                    <div className="description">
+                        Send Me an email at <span>davide.vignozzi@gmail.com</span>
+                    </div>
+                </div>
+            </div>
+
+            <div className="notify">
+                <div className="icon">
+                    <i style={{ backgroundColor: '#2dcb73' }}>
+                        <FiPhoneCall />
+                    </i>
+                </div>
+                <div className="info">
+                    <div className="title">Phone</div>
+                    <div className="description">
+                        Call me at <span>+39 3404596054</span>
+                    </div>
+                </div>
+            </div>
+
+            <div className="notify">
+                <div className="icon">
+                    <i style={{ backgroundColor: '#0077b5' }}>
+                        <FaLinkedin />
+                    </i>
+                </div>
+                <div className="info">
+                    <div className="title">LinkedIn</div>
+                    <div className="description">
+                        Message me on <span>LinkedIn</span>
+                    </div>
+                </div>
+            </div>
+
+            <div className="notify">
+                <div className="icon">
+                    <i style={{ backgroundColor: '#ffffff', color: '#333333' }}>
+                        <FiGithub />
+                    </i>
+                </div>
+                <div className="info">
+                    <div className="title">GitHub</div>
+                    <div className="description">
+                        Message me on <span>GitHub</span>
+                    </div>
+                </div>
+            </div>
+        </Screen>
+    );
+};
+
+export default PhoneScreen;
