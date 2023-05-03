8cddbe7d8786efbb77679271e034c02b9b70ca2a
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-05-03 12:23:19 +0200

    :sparkles: Education Trigger On click

--- src/components/elements/sections/trophies/Trophies.js
+++ src/components/elements/sections/trophies/Trophies.js
@@ -1,11 +1,34 @@
+import { useControls } from 'leva';
+import useInteractions from '../../../../utils/stores/useInteractions';
 import Boolean from './Boolean';
 import Meta from './Meta';
 import ThreeJourney from './ThreeJourney';
 import Udemy from './Udemy';
 
 const Trophies = () => {
+    const state = useInteractions((state) => state);
+
+    const handleClick = () => {
+        if (state.phase !== 'education') {
+            state.education();
+        }
+    };
+
+    const { visible } = useControls('EducationTrigger', {
+        visible: false
+    });
+
     return (
         <group>
+            <mesh
+                scale={[0.76, 0.3, 0.25]}
+                position={[-0.945, 1.6, 2.22]}
+                visible={visible}
+                onClick={handleClick}
+            >
+                <boxGeometry />
+                <meshBasicMaterial wireframe />
+            </mesh>
             <Boolean />
             <ThreeJourney />
             <Meta />
