cc7ad186a43588127eeeaa024e725d319de6ceca
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-05-15 11:28:07 +0200

    :heavy_minus_sign: react-detect-device

--- src/components/LaptopScene.js
+++ src/components/LaptopScene.js
@@ -1,5 +1,5 @@
-import { Html, Plane } from '@react-three/drei';
 import { useRef } from 'react';
+import { Html } from '@react-three/drei';
 import LoadingLaptop from './HTML/LoadingLaptop';
 
 const LaptopScene = (props) => {
@@ -28,6 +28,7 @@ const LaptopScene = (props) => {
 
             {/* LaptopScreen */}
             <mesh
+                ref={screenRef}
                 geometry={nodes.LaptopScreen.geometry}
                 position={nodes.LaptopScreen.position}
                 rotation={nodes.LaptopScreen.rotation}
@@ -39,28 +40,14 @@ const LaptopScene = (props) => {
                     center
                     transform
                     occlude
+                    parent={screenRef.current}
                     distanceFactor={0.06}
-                    position={[-0.005, 0, -0.005]}
-                    rotation={[0.49, Math.PI + 0.39, 0.21]}
+                    position={[-0.005, 0, -0.0025]}
+                    rotation={[0.4875, Math.PI + 0.39, 0.205]}
                 >
                     <LoadingLaptop />
                 </Html>
             </mesh>
-
-            {/* <Plane
-                ref={screenRef}
-                args={[0.3, 0.2]}
-                position={[
-                    nodes.LaptopScreen.position.x,
-                    nodes.LaptopScreen.position.y + 0.005,
-                    nodes.LaptopScreen.position.z - 0.001
-                ]}
-                rotation={[0.49, Math.PI + 0.39, 0.21]}
-                material-color="red"
-                // material={screenMaterial}
-            >
-                <Html fixed center transform distanceFactor={0.1}></Html>
-            </Plane> */}
         </group>
     );
 };
