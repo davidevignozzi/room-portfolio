50e9ceb9519355e41f794334df4a34b75199f02f
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-05-17 17:06:15 +0200

    :lipstick: Tablet-Skills UI

---
+++ src/components/HTML/SkillsTablet.js
@@ -0,0 +1,50 @@
+import {
+    FaHtml5,
+    FaCss3Alt,
+    FaSass,
+    FaBootstrap,
+    FaVuejs,
+    FaReact,
+    FaGitAlt
+} from 'react-icons/fa';
+import { SiTailwindcss, SiJavascript, SiWebgl, SiThreedotjs, SiGreensock } from 'react-icons/si';
+
+const SkillsTablet = () => {
+    /**
+     * Array Of Skills
+     */
+    const skillsArray = [
+        { name: 'HTML', icon: <FaHtml5 className="ico" />, color: '#e34c26' },
+        { name: 'CSS', icon: <FaCss3Alt className="ico" />, color: '#264de4' },
+        { name: 'SCSS', icon: <FaSass className="ico" />, color: '#c69' },
+        { name: 'Bootstrap', icon: <FaBootstrap className="ico" />, color: '#563d7c' },
+        { name: 'Tailwind', icon: <SiTailwindcss className="ico" />, color: '#65bcf4' },
+        { name: 'Javascript', icon: <SiJavascript className="ico" />, color: '#f0db4f' },
+        { name: 'Vue', icon: <FaVuejs className="ico" />, color: '#42b883 ' },
+        { name: 'React', icon: <FaReact className="ico" />, color: '#7cc5d9' },
+        { name: 'GSAP', icon: <SiGreensock className="ico" />, color: '#7ca42e' },
+        { name: 'WebGL', icon: <SiWebgl className="ico" />, color: '#fa3d22' },
+        { name: 'ThreeJs', icon: <SiThreedotjs className="ico" />, color: '#333333' },
+        { name: 'Three-Fiber', icon: <SiThreedotjs className="ico" />, color: '#333333' },
+        { name: 'Git', icon: <FaGitAlt className="ico" />, color: '#f34f29' }
+    ];
+    return (
+        <section id="tablet" className="fonted">
+            <h1 className="skills-title"></h1>
+            <div className="skills">
+                {skillsArray.map((skill, index) => {
+                    return (
+                        <div key={index} className="skill">
+                            <div className="skill-icon" style={{ color: skill.color }}>
+                                {skill.icon}
+                            </div>
+                            <h2 className="skill-name">{skill.name}</h2>
+                        </div>
+                    );
+                })}
+            </div>
+        </section>
+    );
+};
+
+export default SkillsTablet;
--- src/components/TabletScene.js
+++ src/components/TabletScene.js
@@ -1,10 +1,14 @@
+import { Html, Plane } from '@react-three/drei';
+import { useRef } from 'react';
 import useInteractions from '../utils/stores/useInteractions';
+import SkillsTablet from './HTML/SkillsTablet';
 
 const TabletScene = (props) => {
     const nodes = props.nodes;
     const bakedMaterial = props.material;
     const screenMaterial = props.screenMaterial;
     const state = useInteractions((state) => state);
+    const screenRef = useRef();
 
     return (
         <group>
@@ -21,6 +25,7 @@ const TabletScene = (props) => {
 
             {/* TabletScreen */}
             <mesh
+                ref={screenRef}
                 geometry={nodes.WacomCintiqScreen.geometry}
                 position={nodes.WacomCintiqScreen.position}
                 rotation={nodes.WacomCintiqScreen.rotation}
@@ -29,7 +34,23 @@ const TabletScene = (props) => {
                 onClick={() => {
                     state.skills();
                 }}
-            />
+            >
+                <Html
+                    fixed
+                    center
+                    transform
+                    // occlude
+                    parent={screenRef.current}
+                    zIndexRange={[0, 1]}
+                    distanceFactor={0.045}
+                    position={[-0.005, -0.0025, -0.005]}
+                    rotation={[0.8, Math.PI, 0]}
+                    style={{ backfaceVisibility: 'none' }}
+                    // pointerEvents="none"
+                >
+                    <SkillsTablet />
+                </Html>
+            </mesh>
         </group>
     );
 };
