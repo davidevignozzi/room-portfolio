a502eb5ddb1e8fbb7862b30af3f550e364122fd3
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-05-17 12:48:51 +0200

    :lipstick: UI of Main content in Project section

--- src/components/HTML/ProjectsMonitor.js
+++ src/components/HTML/ProjectsMonitor.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { FcFolder } from 'react-icons/fc';
+import { FaGithub, FaTag } from 'react-icons/fa';
 
 /**
  * Projects Array
@@ -9,10 +9,17 @@ const projectsArray = [
         id: 1,
         name: 'Marble Race',
         ref: 'https://r3f-marble-race.vercel.app/',
+        code: 'https://github.com/davidevignozzi/threejs-journey/tree/main/react-three-fiber/r3f-jumping-sphere-game',
         img: './assets/images/MarbleRace.png',
         description:
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti recusandae ipsam sint blanditiis odit. Libero eius officia perspiciatis iste ipsam quaerat, ex veniam consequatur alias saepe, unde eligendi error iusto? Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti recusandae ipsam sint blanditiis odit. Libero eius officia perspiciatis iste ipsam quaerat, ex veniam consequatur alias saepe, unde eligendi error iusto?',
-        tec: ['html', 'css', 'react', 'three', 'react-three-fiber']
+        tec: [
+            { name: 'html', bg: '#e34c26', color: '#f0f0f0' },
+            { name: 'css', bg: '#264de4', color: '#f0f0f0' },
+            { name: 'react', bg: '#7cc5d9', color: '#f0f0f0' },
+            { name: 'three', bg: '#6c63eb', color: '#f0f0f0' },
+            { name: 'react-three-fiber', bg: '#6c63eb', color: '#f0f0f0' }
+        ]
     },
     {
         id: 2,
@@ -21,7 +28,13 @@ const projectsArray = [
         img: './assets/images/MarbleRace.png',
         description:
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti recusandae ipsam sint blanditiis odit. Libero eius officia perspiciatis iste ipsam quaerat, ex veniam consequatur alias saepe, unde eligendi error iusto? Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti recusandae ipsam sint blanditiis odit. Libero eius officia perspiciatis iste ipsam quaerat, ex veniam consequatur alias saepe, unde eligendi error iusto?',
-        tec: ['html', 'css', 'react', 'three', 'react-three-fiber']
+        tec: [
+            { name: 'html', bg: '#e34c26', color: '#f0f0f0' },
+            { name: 'css', bg: '#264de4', color: '#f0f0f0' },
+            { name: 'react', bg: '#7cc5d9', color: '#f0f0f0' },
+            { name: 'three', bg: '#6c63eb', color: '#f0f0f0' },
+            { name: 'react-three-fiber', bg: '#6c63eb', color: '#f0f0f0' }
+        ]
     },
     {
         id: 3,
@@ -31,7 +44,13 @@ const projectsArray = [
         img: './assets/images/MarbleRace.png',
         description:
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti recusandae ipsam sint blanditiis odit. Libero eius officia perspiciatis iste ipsam quaerat, ex veniam consequatur alias saepe, unde eligendi error iusto? Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti recusandae ipsam sint blanditiis odit. Libero eius officia perspiciatis iste ipsam quaerat, ex veniam consequatur alias saepe, unde eligendi error iusto?',
-        tec: ['html', 'css', 'react', 'three', 'react-three-fiber']
+        tec: [
+            { name: 'html', bg: '#e34c26', color: '#f0f0f0' },
+            { name: 'css', bg: '#264de4', color: '#f0f0f0' },
+            { name: 'react', bg: '#7cc5d9', color: '#f0f0f0' },
+            { name: 'three', bg: '#6c63eb', color: '#f0f0f0' },
+            { name: 'react-three-fiber', bg: '#6c63eb', color: '#f0f0f0' }
+        ]
     }
 ];
 
@@ -39,7 +58,7 @@ const ProjectsMonitor = () => {
     const [selectedProject, setSelectedProject] = useState(projectsArray[0]);
 
     return (
-        <section id="monitor">
+        <section id="monitor" className="fonted">
             <div className="monitor-window-container">
                 <div className="window">
                     <div className="window--tob-bar">
@@ -68,6 +87,44 @@ const ProjectsMonitor = () => {
                                     );
                                 })}
                             </ul>
+                        </div>
+                        <div className="window-content-main">
+                            <div className="window-content-main-img">
+                                <img src={selectedProject.img} alt={selectedProject.name} />
+                            </div>
+                            <h1 className="window-content-main-title">{selectedProject.name}</h1>
+                            <div className="window-content-main-tags">
+                                <ul>
+                                    {selectedProject.tec.map((tag, index) => {
+                                        return (
+                                            <li
+                                                key={index}
+                                                // style={{
+                                                //     color: tag.color,
+                                                //     backgroundColor: tag.bg
+                                                // }}
+                                            >
+                                                <FaTag />
+                                                <span>{tag.name}</span>
+                                            </li>
+                                        );
+                                    })}
+                                    <div className="ribbon"></div>
+                                </ul>
+                            </div>
+                            <div className="window-content-main-description">
+                                {selectedProject.description}
+                            </div>
+                            <div className="window-content-main-buttons">
+                                <button className="window-content-main-code bg-secondary">
+                                    <a href={selectedProject.code}>
+                                        <FaGithub /> <span>Source Code</span>
+                                    </a>
+                                </button>
+                                <button className="window-content-main-ref bg-blue">
+                                    <a href={selectedProject.ref}>Open It</a>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
